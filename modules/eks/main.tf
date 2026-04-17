334e2a4e54b1eea8a4ccee0343884daf27d4ec4c
saranca14 <61488445+saranca14@users.noreply.github.com>
2026-04-17 22:53:07 +0200

    chore: upgrade ECR policy from ReadOnly to PowerUser for EKS nodes

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -73,8 +73,8 @@ resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
   role       = aws_iam_role.eks_nodes.name
 }
 
-resource "aws_iam_role_policy_attachment" "ecr_read_only" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+resource "aws_iam_role_policy_attachment" "ecr_power_user" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"
   role       = aws_iam_role.eks_nodes.name
 }
 
@@ -107,7 +107,7 @@ resource "aws_eks_node_group" "main" {
   depends_on = [
     aws_iam_role_policy_attachment.eks_worker_node_policy,
     aws_iam_role_policy_attachment.eks_cni_policy,
-    aws_iam_role_policy_attachment.ecr_read_only,
+    aws_iam_role_policy_attachment.ecr_power_user,
   ]
 }
 
